b29dde2b7fee7e58d583d561df1987c8681e04a3
Christian <christiannguyendinh@yahoo.com>
2017-01-16 19:52:53 -0500

    Added support for multiple tables in a database. GUI and backend now use the DataTable object for storing and passing information about a table. Added more information about tables in the GUI including row/col count, table name, and types of each column

--- src/main/ServerConnection.java
+++ src/main/ServerConnection.java
@@ -9,55 +9,59 @@ import java.sql.Statement;
 import java.util.ArrayList;
 
 public class ServerConnection {
-
+	/*
+	// Debugging + Testing
 	public static void main(String[] args) throws ClassNotFoundException{
-		Object[] data = accessDatabase();
-		String[] cols = (String[]) data[0];
-		Object[][] d = (Object[][]) data[1];
+		DataTable[] data = accessDatabase();
 		
-		for (int i = 0; i < d.length; i++) {
-			for (int j = 0; j < d[0].length; j++) {
-				System.out.print(d[i][j] + " ");
+		for (int i = 0; i < data.length; i++) {
+			System.out.println(data[i].tableName);
+			System.out.println(data[i].numRows);
+			System.out.println(data[i].numCols);
+			System.out.println(data[i].getTypesArrayString());
+			
+			System.out.println("Col Names:");
+			String[] cn = data[i].getColumnNames();
+			for (int j = 0; j < cn.length; j++) {
+				System.out.println(cn[j]);
 			}
-			System.out.println();
-		}
-		System.out.println();
-		for (int i = 0; i < cols.length; i++) {
-			System.out.println(cols[i]);
+			System.out.println("Table:");
+			Object[][] d = data[i].getData();
+			for (int j = 0; j < d.length; j++) {
+				for (int k = 0; k < d[0].length; k++) {
+					System.out.print(d[j][k] + " ");
+				}	
+				System.out.println();
+			}
+			System.out.println("===================");
 		}
 	}
-	
-	private enum SQLiteDataTypes {
-		NULL, INTEGER, REAL, TEXT, BLOB;
-	}
+	*/
 	
 	/**
 	 * Connect to a database, CURRENTLY HARDCODED, then return the data stored
 	 * there in the form of an array for parsing by the gui
-	 * @return An array of size 2. First element is an String array of column names,
-	 * Second is a 2D Object array of the table data.
+	 * @return An array of DataTable objects with length according to number
+	 * of tables found in the database.
 	 * @throws ClassNotFoundException
 	 */
-	public static Object[] accessDatabase() throws ClassNotFoundException {
+	public static DataTable[] accessDatabase() throws ClassNotFoundException {
 		Class.forName("org.sqlite.JDBC");
 		
 		Connection connection = null;
-		Object[] returnArr = new Object[2];
+		// List of DataTable objects corresponding to each table
+		DataTable[] DTarray = null;
+		
 		try {
 			connection = DriverManager.getConnection("jdbc:sqlite:/Users/christian/Documents/workspace/SQLGUI/src/main/test.db");
 			Statement statement = connection.createStatement();
 			statement.setQueryTimeout(15);
 			
 			ResultSet rs = null;
+			// List of tables
 			ArrayList<String> tables = new ArrayList<String>();
-			// Will contain data for the whole table
-			Object[][] data = null;
-			// Column names for the table
-			String[] columnNames = null;
-			// Types for loading the SQL data into an object
-			SQLiteDataTypes[] types = null;
 			
-			// GET TABLE NAMES - CURRENTLY ONLY WORKS WITH ONE TABLE!
+			// GET TABLE NAMES
 			rs = connection.getMetaData().getTables(null, null, null, new String[] {"TABLE"});
 			while(rs.next()) {
 				// sqlite_sequence is created when a table is created and uses AUTOINCREMENT
@@ -65,84 +69,12 @@ public class ServerConnection {
 				if (!name.equals("sqlite_sequence"))
 					tables.add(name);
 			}
-			
-			// Load data for table
-			for (String t : tables) {
-				// Get the number of rows
-				int numRows = 0;
-				rs = statement.executeQuery("SELECT COUNT(*) FROM " + t);
-				// Should only run once
-				while (rs.next()) {
-					numRows = rs.getInt(1);
-				}
-				// Get the number of columns
-				int numCols = 0;
-				rs = statement.executeQuery("SELECT * FROM " + t + " LIMIT 1");
-				ResultSetMetaData rsmd = rs.getMetaData();
-				numCols = rsmd.getColumnCount();
-				
-				data = new Object[numRows][numCols];
-				columnNames = new String[numCols];
-				types = new SQLiteDataTypes[numCols];
-				
-				
-				// Populate the columns
-				rs = connection.getMetaData().getColumns(null, null, t, "%");
-				int index = 0;
-				while(rs.next()) {
-					columnNames[index] = rs.getString("COLUMN_NAME");
-					String type_name = rs.getString("TYPE_NAME");
-					switch (type_name) {
-						case "NULL": 	types[index] = SQLiteDataTypes.NULL;
-										break;
-						case "INTEGER": types[index] = SQLiteDataTypes.INTEGER;
-										break;
-						case "REAL": 	types[index] = SQLiteDataTypes.REAL;
-										break;
-						case "TEXT": 	types[index] = SQLiteDataTypes.TEXT;
-										break;
-						case "BLOB": 	types[index] = SQLiteDataTypes.BLOB;
-										break;
-						default:		System.err.println("Something went terribly wrong with the types.");
-										break;
-						
-					}
-					index++;
-				}
-				
-				// Load the data into the 2d data array
-				rs = statement.executeQuery("SELECT * FROM " + t);
-				index = 0;
-				while(rs.next()) {
-					Object[] row = new Object[numCols];
-					// Fill in the row with data from the SELECT statement
-					for (int i = 0; i < numCols; i++) {
-						String colName = columnNames[i];
-						Object d = null;
-						// Create each data point based on its type
-						switch (types[i]) {
-							case INTEGER: 	d = new Integer(rs.getInt(colName));
-											break;
-							case REAL: 		d = new Double(rs.getDouble(colName));
-											break;
-							case TEXT: 		d = rs.getString(colName);
-											break;
-							case BLOB: 		d = rs.getObject(colName);
-											break;
-							default: 		d = null;
-											break;
-						}
-						// Assemble the row
-						row[i] = d;
-					}
-					// Assemble the table data of rows
-					data[index] = row;
-					index++;
-				}
+
+			DTarray = new DataTable[tables.size()];
+			// Load data for each table
+			for (int i = 0; i < tables.size(); i++) {
+				DTarray[i] = populateDataTables(tables.get(i), statement, rs, connection);
 			}
-			// Prepare the return array
-			returnArr[0] = columnNames;
-			returnArr[1] = data;
 			
 		} catch (SQLException e) {
 			System.err.println(e.getMessage());
@@ -155,8 +87,110 @@ public class ServerConnection {
 			}
 		}
 		
-		// Return null if error or data if success
-		return returnArr;
+		// Return null if error or data if success. MAKE THIS AN OBJECT LATER
+		return DTarray;
+	}
+	
+	/**
+	 * Create The DataTable objects that contain data about each table
+	 * @param tableName - String
+	 * @param statement - Statement Object
+	 * @param rs - ResultSet Object
+	 * @param connection - Connection Object
+	 * @return DataTable object with info about parameter table
+	 * @throws SQLException
+	 */
+	private static DataTable populateDataTables(String tableName, Statement statement, 
+			ResultSet rs, Connection connection) throws SQLException {
+		
+		DataTable dt = new DataTable();
+		// Will contain data for the whole table
+		Object[][] data = null;
+		// Column names for the table
+		String[] columnNames = null;
+		// Types for loading the SQL data into an object
+		SQLiteDataTypes[] types = null;
+		int numRows = 0;
+		int numCols = 0;
+		
+		// Get the number of rows
+		rs = statement.executeQuery("SELECT COUNT(*) FROM " + tableName);
+		// Should only run once
+		while (rs.next()) {
+			numRows = rs.getInt(1);
+		}
+		// Get the number of columns
+		rs = statement.executeQuery("SELECT * FROM " + tableName + " LIMIT 1");
+		ResultSetMetaData rsmd = rs.getMetaData();
+		numCols = rsmd.getColumnCount();
+		
+		data = new Object[numRows][numCols];
+		columnNames = new String[numCols];
+		types = new SQLiteDataTypes[numCols];
+		
+		
+		// Populate the columns
+		rs = connection.getMetaData().getColumns(null, null, tableName, "%");
+		int index = 0;
+		while(rs.next()) {
+			columnNames[index] = rs.getString("COLUMN_NAME");
+			String type_name = rs.getString("TYPE_NAME");
+			switch (type_name) {
+				case "NULL": 	types[index] = SQLiteDataTypes.NULL;
+								break;
+				case "INTEGER": types[index] = SQLiteDataTypes.INTEGER;
+								break;
+				case "REAL": 	types[index] = SQLiteDataTypes.REAL;
+								break;
+				case "TEXT": 	types[index] = SQLiteDataTypes.TEXT;
+								break;
+				case "BLOB": 	types[index] = SQLiteDataTypes.BLOB;
+								break;
+				default:		System.err.println("Something went terribly wrong with the types.");
+								break;
+				
+			}
+			index++;
+		}
+		
+		// Load the data into the 2d data array
+		rs = statement.executeQuery("SELECT * FROM " + tableName);
+		index = 0;
+		while(rs.next()) {
+			Object[] row = new Object[numCols];
+			// Fill in the row with data from the SELECT statement
+			for (int i = 0; i < numCols; i++) {
+				String colName = columnNames[i];
+				Object d = null;
+				// Create each data point based on its type
+				switch (types[i]) {
+					case INTEGER: 	d = new Integer(rs.getInt(colName));
+									break;
+					case REAL: 		d = new Double(rs.getDouble(colName));
+									break;
+					case TEXT: 		d = rs.getString(colName);
+									break;
+					case BLOB: 		d = rs.getObject(colName);
+									break;
+					default: 		d = null;
+									break;
+				}
+				// Assemble the row
+				row[i] = d;
+			}
+			// Assemble the table data of rows
+			data[index] = row;
+			index++;
+		}
+		
+		// Create the data table object to return
+		dt.tableName = tableName;
+		dt.setColumnNames(columnNames);
+		dt.setData(data);
+		dt.numRows = numRows;
+		dt.numCols = columnNames.length;
+		dt.setTypesArrayString(types);
+		return dt;
 	}
 
 }
--- src/main/ServerGUI.java
+++ src/main/ServerGUI.java
@@ -16,6 +16,9 @@ import javax.swing.JScrollPane;
 import javax.swing.JButton;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import javax.swing.JLabel;
+import javax.swing.event.ChangeListener;
+import javax.swing.event.ChangeEvent;
 
 public class ServerGUI {
 
@@ -55,8 +58,15 @@ public class ServerGUI {
 			{new Integer(11), "qq"},
 	};*/
 	
-	private static String[] columnNames = null;
-	private static Object[][] data = null;
+	//private static String[] columnNames = null;
+	//private static Object[][] data = null;
+	//private static int numRows = 0;
+	//private static int numCols = 0;
+	//private static String typesArrayString = null;
+	
+	// Array containing dataTable objects corresponding to each table in the DB
+	private static DataTable[] tables = null;
+	
 
 	/**
 	 * Launch the application.
@@ -71,9 +81,10 @@ public class ServerGUI {
 					//		- Have a separate method that returns the number of tables found
 					//		- Then do this to get a dataset for each table
 					//		- Then make a method HERE to add a table tab to the gui
-					Object[] table_data = ServerConnection.accessDatabase();
-					columnNames = (String[]) table_data[0];
-					data = (Object[][]) table_data[1];
+					
+					// Create the datatable for display
+					tables = ServerConnection.accessDatabase();
+					
 					
 					ServerGUI window = new ServerGUI();
 					window.frame.setVisible(true);
@@ -104,18 +115,19 @@ public class ServerGUI {
 		JPanel panel = new JPanel();
 		frame.getContentPane().add(panel, BorderLayout.CENTER);
 		GridBagLayout gbl_panel = new GridBagLayout();
-		gbl_panel.columnWidths = new int[]{0, 475, 0, 0};
+		gbl_panel.columnWidths = new int[]{0, 0, 475, 0, 0};
 		gbl_panel.rowHeights = new int[]{0, 0, 0, 0};
-		gbl_panel.columnWeights = new double[]{0.0, 1.0, 0.0, Double.MIN_VALUE};
-		gbl_panel.rowWeights = new double[]{0.0, 1.0, 0.0, Double.MIN_VALUE};
+		gbl_panel.columnWeights = new double[]{0.0, 0.0, 1.0, 0.0, Double.MIN_VALUE};
+		gbl_panel.rowWeights = new double[]{1.0, 0.0, 0.0, Double.MIN_VALUE};
 		panel.setLayout(gbl_panel);
 		
 		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
 		GridBagConstraints gbc_tabbedPane = new GridBagConstraints();
-		gbc_tabbedPane.insets = new Insets(0, 0, 5, 5);
+		gbc_tabbedPane.gridwidth = 4;
+		gbc_tabbedPane.insets = new Insets(0, 0, 5, 0);
 		gbc_tabbedPane.fill = GridBagConstraints.BOTH;
-		gbc_tabbedPane.gridx = 1;
-		gbc_tabbedPane.gridy = 1;
+		gbc_tabbedPane.gridx = 0;
+		gbc_tabbedPane.gridy = 0;
 		panel.add(tabbedPane, gbc_tabbedPane);
 		
 		JPanel panel_3 = new JPanel();
@@ -123,14 +135,31 @@ public class ServerGUI {
 		
 		// Main Panel, Panel 1 -----------------------------------------
 		
-		JPanel panel_1 = new JPanel();
-		tabbedPane.addTab("Tables", null, panel_1, null);
-		panel_1.setLayout(new BorderLayout(0, 0));
+		for (int i = 0; i < tables.length; i++) {
+			createTableTabs(tabbedPane, tables[i]);
+		}
 		
-		JScrollPane scrollPane = new JScrollPane();
-		panel_1.add(scrollPane);
-		JTable table = new JTable(data, columnNames);
-		scrollPane.setViewportView(table);
+		JLabel lblRows = new JLabel("# Rows: ");
+		GridBagConstraints gbc_lblRows = new GridBagConstraints();
+		gbc_lblRows.anchor = GridBagConstraints.WEST;
+		gbc_lblRows.insets = new Insets(0, 0, 5, 5);
+		gbc_lblRows.gridx = 1;
+		gbc_lblRows.gridy = 1;
+		panel.add(lblRows, gbc_lblRows);
+		// By default the tables wont be showing
+		lblRows.setVisible(false);
+		
+		JLabel lblCols = new JLabel("# Cols: ");
+		lblCols.setToolTipText("The types for each col are: ");
+		GridBagConstraints gbc_lblCols = new GridBagConstraints();
+		gbc_lblCols.anchor = GridBagConstraints.WEST;
+		gbc_lblCols.insets = new Insets(0, 0, 0, 5);
+		gbc_lblCols.gridx = 1;
+		gbc_lblCols.gridy = 2;
+		panel.add(lblCols, gbc_lblCols);
+		// By default the tables wont be showing
+		lblCols.setVisible(false);
+		
 		
 		// Main Panel, Panel 2 ---------------------------------------
 		
@@ -138,13 +167,47 @@ public class ServerGUI {
 		tabbedPane.addTab("Query", null, panel_2, null);
 		
 		JButton btnNewButton = new JButton("New button");
+		panel_2.add(btnNewButton);
+		
+		// Event Handlers
+		
 		btnNewButton.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				data[0][0] = new Integer(50);
+				//dt.getData()[0][0] = new Integer(50);
+				System.out.println("button clicked");
 			}
 		});
-		panel_2.add(btnNewButton);
+		
+		tabbedPane.addChangeListener(new ChangeListener() {
+			public void stateChanged(ChangeEvent e) {
+				// May need to move to a range with multiple tables
+				int index = tabbedPane.getSelectedIndex();
+				// First and last indexes are not table tabs
+				if (index != 0 && index != tabbedPane.getComponentCount() - 1) {
+					lblRows.setVisible(true);
+					// First tab is the DB tab, so table tabs will be ahead by 1
+					lblRows.setText("# Rows: " + tables[index - 1].numRows);
+					lblCols.setVisible(true);
+					lblCols.setText("# Cols: " + tables[index - 1].numCols);
+					lblCols.setToolTipText("The Types for each Column are: " + tables[index - 1].getTypesArrayString());
+				} else {
+					lblRows.setVisible(false);
+					lblCols.setVisible(false);
+				}
+			}
+		});
+	}
+	
+	private void createTableTabs(JTabbedPane tabbedPane, DataTable dt) {
+		JPanel panel_1 = new JPanel();
+		tabbedPane.addTab(dt.tableName, null, panel_1, null);
+		panel_1.setLayout(new BorderLayout(0, 0));
+		
+		JScrollPane scrollPane = new JScrollPane();
+		panel_1.add(scrollPane);
+		JTable table = new JTable(dt.getData(), dt.getColumnNames());
+		scrollPane.setViewportView(table);
 	}
 
 }
